bffff8c8972d06b7fb166e91fe823e3930dab59b
umer <mumarkhan383@gmail.com>
2024-12-16 17:45:59 +0500

    themechanges

--- src/app/thank-you-order/AfterPaymentThank.tsx
+++ src/app/thank-you-order/AfterPaymentThank.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { CheckCheckIcon, HomeIcon } from 'lucide-react'
+import { CheckCheckIcon, CheckIcon, Home, HomeIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
@@ -22,18 +22,18 @@ function AfterPaymentThank() {
 
 
   return (
-    <div className="flex  flex-col items-center justify-center h-screen bg-gradient-to-t from-sky-200 via-purple-200 to-zinc-100 dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] px-4 pb-10 pt-32 md:pt-2 sm:px-6 lg:px-8">
+    <div className="flex  flex-col items-center justify-center h-screen bg-gradient-to-b from-[#a9d6e5] via-[#89c2d9] to-[#eef4ed] dark:bg-gradient-to-b dark:from-[#03045e] dark:via-[#1d1e4e] dark:to-[#2a6f97] px-4 pb-10 pt-32 md:pt-2 sm:px-6 lg:px-8 text-zinc-800 dark:text-zinc-100">
       <div className="mx-auto max-w-screen-xl text-center scale-90 md:scale-100">
         <div className='grid grid-cols-1 gap-4 '>
           <div className='flex justify-center items-center '>
-            <div className="flex items-center justify-center rounded-full bg-violet-600 px-4 py-2 font-semibold text-zinc-200">
-              <CheckCheckIcon className="mr-2 h-5 w-5" />
+            <div className="flex items-center justify-center rounded-full bg-blue-600 px-4 py-2 font-semibold text-zinc-200">
+              <CheckIcon className="mr-2 h-5 w-5" />
               <span>Thank You for Your Order and Payment!</span>
             </div>
           </div>
           <div className='flex justify-center items-center '>
             <div className="m flex items-center justify-center rounded-full bg-green-600 px-4 py-2 font-semibold text-zinc-200">
-              <CheckCheckIcon className="mr-2 h-5 w-5" />
+              <CheckIcon className="mr-2 h-5 w-5" />
               <span>We Have Recieved Your Payment</span>
             </div>
           </div>
@@ -48,12 +48,10 @@ function AfterPaymentThank() {
           Thank you for your order! Enjoy a special 40% discount. Check WhatsApp at <span className='font-bold'>{phone}</span> for details!
         </h1>
         <p className="md:mt-4 mt-2 lg:px-52 px-10">
-          Thank you for your order and payment! We’re excited to have you with us and are already working to fulfill your request.
-
-          A confirmation email with your order and payment details has been sent to you. If you have any questions or need assistance, please feel free to reach out. Our team is here to ensure you have a seamless experience. Thank you for trusting us with your order!</p>
-        <div className="mt-6 flex flex-col items-center md:flex-row justify-center gap-4">
+      Thank you for your payment! We're thrilled to have you onboard and are already preparing to assist with your online exam. A confirmation email with your payment and exam details has been sent to you. If you have any questions or need support, don't hesitate to reach out. Our team is here to make sure your experience is smooth and hassle-free. Thank you for choosing us to help with your online exam!</p>
+        <div className="mt-4 flex flex-col items-center md:flex-row justify-center gap-4">
           <Link href="/" className="flex items-center justify-center hover:scale-105 transition ease-in duration-200 delay-200 py-3 bg-zinc-200 dark:bg-zinc-900 w-[200px] text-black dark:text-zinc-100 border-[2px] border-black dark:border-white rounded-lg" prefetch={false} >
-            <HomeIcon className='mr-2' /> Return To Home
+            <Home className='mr-2' /> Return To Home
           </Link>
         </div>
       </div>
--- src/app/thanks/ThankYou.tsx
+++ src/app/thanks/ThankYou.tsx
@@ -1,43 +1,27 @@
 "use client"
-import Link from 'next/link'
 import React from 'react'
-import { Package2 } from 'lucide-react'
-import Image from 'next/image'
+import { CtaButtons } from '../(Home)/Banner'
 
 function ThankYou() {
     const name = localStorage.getItem("name")
     const number = localStorage.getItem("number");
     const discount = localStorage.getItem("discount")
   return (
-    <div className="flex  flex-col items-center justify-center h-screen bg-gradient-to-t from-sky-200 via-purple-200 to-zinc-100 dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] px-4 pb-10 pt-32 md:pt-2 sm:px-6 lg:px-8">
+    <div className="flex  flex-col items-center justify-center h-screen bg-gradient-to-b from-[#a9d6e5] via-[#89c2d9] to-[#eef4ed] dark:bg-gradient-to-b dark:from-[#03045e] dark:via-[#1d1e4e] dark:to-[#2a6f97] px-4 pb-10 pt-32 md:pt-2 sm:px-6 lg:px-8 text-zinc-800 dark:text-zinc-100">
     <div className="mx-auto max-w-screen-xl text-center scale-90 md:scale-100">
-      <div className="inline-flex items-center rounded-full bg-violet-500  px-4 py-2 text-zinc-100">
+      <div className="inline-flex items-center rounded-full bg-blue-500  px-4 py-2 text-zinc-100">
         <CheckIcon className="mr-2 h-5 w-5" />
         <span>Thank You for Reaching Out! </span>
       </div>
 
       <h1 className="mt-6 text-lg font-medium  sm:text-xl">
-        Please Check your Whatsapp {number}  we have sent you {discount ? '55%': '40%'} discount offer
+        Please Check your Whatsapp {number}  we have sent you {discount ? '55%': '50%'} discount offer
       </h1>
-      <p className="mt-4  mx-auto px-10 lg:px-52">
-      Thank you for your interest in our services. Our team of dedicated experts is committed to providing an exceptional experience for our valued students.We know that our success is linked to our students’ achievements, which is why we consistently strive to go above and beyond. Every task we take on is completed with a focus on timely delivery, transparent budgeting, and the highest standards of quality
+      <p className="mt-4  mx-auto px-10 lg:px-52">     
+Thank you for considering our online exam relief services. Our team of dedicated experts is committed to ensuring a seamless and stress-free experience for students. We understand that your success is closely tied to your performance, which is why we go the extra mile to support you. Each exam we handle is approached with a focus on timely assistance, clear pricing, and delivering top-notch quality to help you succeed.
       </p>
-      <div className="mt-6 flex flex-col items-center md:flex-row justify-center gap-4">
-
-      <Link href={'https://wa.me/3197005034911?text=Hi, I urgently need academic assistance. Could you help me submit my assignment before the deadline?'} target="_blank" rel="noopener noreferrer">
-      <div className='flex items-center justify-center hover:scale-105 transition ease-in duration-200 delay-200 py-3 bg-zinc-200 dark:bg-zinc-900 w-[220px] border-[2px] border-black dark:border-white rounded-lg'>
-        <Image src={"/imgs/whatsapp.png"} width={25} height={25} alt='whatsapp' />
-        <button className='font-medium ml-2 text-background md:text-base text-sm text-black dark:text-zinc-100 '>Chat On Whatsapp</button>
-      </div>
-      </Link>
-
-        <Link
-          href="/order-now"
-          className="flex items-center justify-center hover:scale-105 transition ease-in duration-200 delay-200 py-3 bg-zinc-200 dark:bg-zinc-900 w-[200px] text-black dark:text-zinc-100 border-[2px] border-black dark:border-white rounded-lg"
-          prefetch={false}
-        >
-           <Package2 className='mr-2'/> Order Now
-        </Link>
+      <div className="mt-2 flex flex-col items-center md:flex-row justify-center gap-4">
+        <CtaButtons/>
       </div>
     </div>
   </div>
